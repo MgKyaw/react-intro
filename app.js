7b16be62dc518055f46b1328e17171c03b1e625a
MgKyaw <mgkyaw20112@gmail.com>
2025-02-25 23:17:45 +0700

    feat: Comment p1 and p2

--- app.js
+++ app.js
@@ -5,5 +5,5 @@
 
 // const myArticle = <article></article>;
 
-const p1 = <p id="large">foo</p>;
-const p2 = <p id="small">bar</p>;
+// const p1 = <p id="large">foo</p>;
+// const p2 = <p id="small">bar</p>;
