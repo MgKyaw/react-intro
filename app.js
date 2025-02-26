69eb8b776d4a950a3e910ac5ae9180ca8d7cbc20
MgKyaw <mgkyaw20112@gmail.com>
2025-02-26 22:57:26 +0700

    feat: Comment rendering JSX

--- app.js
+++ app.js
@@ -30,10 +30,10 @@
 // const root = createRoot(container);
 // root.render(<h1>Hello world</h1>);    
      
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
 
-// Write code here:
-const container = document.getElementById('container');
-const root = createRoot(container);
-root.render(<h1>Hello world</h1>);
+// // Write code here:
+// const container = document.getElementById('container');
+// const root = createRoot(container);
+// root.render(<h1>Hello world</h1>);
