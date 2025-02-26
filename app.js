593f413e4a0a0e776faa61b95484cc07dd4766ea
MgKyaw <mgkyaw20112@gmail.com>
2025-02-26 22:56:56 +0700

    feat: Render h1 element

--- app.js
+++ app.js
@@ -36,4 +36,4 @@ import { createRoot } from 'react-dom/client';
 // Write code here:
 const container = document.getElementById('container');
 const root = createRoot(container);
-
+root.render(<h1>Hello world</h1>);
