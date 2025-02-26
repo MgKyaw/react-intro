eb2e63b5d88286d01971e5778a8ddc332363d331
MgKyaw <mgkyaw20112@gmail.com>
2025-02-26 22:48:53 +0700

    feat: Comment wrapping elements with outer div

--- app.js
+++ app.js
@@ -10,16 +10,16 @@
 
 // const myDiv = (<div><h1>Hello world</h1></div>);
 
-const blog = (
-    <div>
-      <img src="pics/192940u73.jpg" />
-      <h1>
-        Welcome to Dan's Blog!
-      </h1>
-      <article>
-        Wow I had the tastiest sandwich today. I <strong>literally</strong> almost freaked out.
-      </article>
-    </div>
-  );
+// const blog = (
+//     <div>
+//       <img src="pics/192940u73.jpg" />
+//       <h1>
+//         Welcome to Dan's Blog!
+//       </h1>
+//       <article>
+//         Wow I had the tastiest sandwich today. I <strong>literally</strong> almost freaked out.
+//       </article>
+//     </div>
+//   );
      
      
