bbdcabf2d8137f5ffd061ebb0a0910d4b4f5d38c
MgKyaw <mgkyaw20112@gmail.com>
2025-02-26 22:59:02 +0700

    feat: Add code for passing a variable

--- app.js
+++ app.js
@@ -37,3 +37,12 @@
 // const container = document.getElementById('container');
 // const root = createRoot(container);
 // root.render(<h1>Hello world</h1>);
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+// Write code here:
+
+
