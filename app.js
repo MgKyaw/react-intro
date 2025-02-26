0dccdcae479dba9431945f47c4f4ed3d52193f29
MgKyaw <mgkyaw20112@gmail.com>
2025-02-26 22:53:52 +0700

    feat: Comment render JSX expression

--- app.js
+++ app.js
@@ -22,11 +22,11 @@
 //     </div>
 //   );
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
 
-// Copy code here:
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<h1>Hello world</h1>);    
+// // Copy code here:
+// const container = document.getElementById('app');
+// const root = createRoot(container);
+// root.render(<h1>Hello world</h1>);    
      
