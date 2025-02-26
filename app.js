b7bc2b9e02f215d2f77c0bc5e124c67460d9d3dc
MgKyaw <mgkyaw20112@gmail.com>
2025-02-26 22:54:20 +0700

    feat: Add code for render JSX

--- app.js
+++ app.js
@@ -30,3 +30,8 @@
 // const root = createRoot(container);
 // root.render(<h1>Hello world</h1>);    
      
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Write code here:
+
