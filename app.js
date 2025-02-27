7cfc988aa0c8008b189ba57e1c1434a5dd904088
MgKyaw <mgkyaw20112@gmail.com>
2025-02-27 23:10:02 +0700

    feat: Add code for self-closing tags

--- app.js
+++ app.js
@@ -63,3 +63,18 @@
 // const myDiv = <div className="big">I AM A BIG DIV</div>;
 
 // root.render(myDiv);
+
+const profile = (
+    <div>
+      <h1>John Smith</h1>
+      <img src="images/john.png">
+      <article>
+        My name is John Smith.
+        <br>
+        I am a software developer.
+        <br>
+        I specialize in creating React applications.
+      </article>
+    </div>
+  );
+  
