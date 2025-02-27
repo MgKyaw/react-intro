f298ec6fb76ec58e5f1493a3e3300b04420ac3be
MgKyaw <mgkyaw20112@gmail.com>
2025-02-27 23:15:01 +0700

    feat: Render string in JSX

--- app.js
+++ app.js
@@ -84,3 +84,4 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app');
 const root = createRoot(container);
 // Write code here:
+root.render(<h1>2 + 3</h1>);
