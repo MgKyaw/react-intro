b734d78d6c40ca2bade93ecc7ad7dc32c83933d8
MgKyaw <mgkyaw20112@gmail.com>
2025-02-27 23:08:31 +0700

    feat: Render myDiv element

--- app.js
+++ app.js
@@ -61,3 +61,5 @@ const container = document.getElementById('app');
 const root = createRoot(container);
 // Write code here:
 const myDiv = <div className="big">I AM A BIG DIV</div>;
+
+root.render(myDiv);
