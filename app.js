d1cbfa2ebbdf4e7d5efe3a09c079a19790e20c3d
MgKyaw <mgkyaw20112@gmail.com>
2025-02-27 23:13:20 +0700

    feat: Comment self-closing tags

--- app.js
+++ app.js
@@ -64,16 +64,16 @@
 
 // root.render(myDiv);
 
-const profile = (
-    <div>
-      <h1>John Smith</h1>
-      <img src="images/john.png" />
-      <article>
-        My name is John Smith.
-        <br />
-        I am a software developer.
-        <br />
-        I specialize in creating React applications.
-      </article>
-    </div>
-  );
+// const profile = (
+//     <div>
+//       <h1>John Smith</h1>
+//       <img src="images/john.png" />
+//       <article>
+//         My name is John Smith.
+//         <br />
+//         I am a software developer.
+//         <br />
+//         I specialize in creating React applications.
+//       </article>
+//     </div>
+//   );
