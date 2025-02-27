34ee00a1c170c64d00d75195451e16008b6896d3
MgKyaw <mgkyaw20112@gmail.com>
2025-02-27 23:20:48 +0700

    feat: Add math element

--- app.js
+++ app.js
@@ -93,3 +93,4 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app');
 const root = createRoot(container);
 // Write code here:
+const math = <h1>2 + 3 = 2 + 3</h1>;
