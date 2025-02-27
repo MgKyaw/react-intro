0e4d73fd080d0de2b0bdf34fa210d4d9573f6fbe
MgKyaw <mgkyaw20112@gmail.com>
2025-02-27 23:19:43 +0700

    feat: Add code for Pi

--- app.js
+++ app.js
@@ -86,3 +86,10 @@
 // // Write code here:
 // // root.render(<h1>2 + 3</h1>); // output => 2 + 3
 // root.render(<h1>{2 + 3}</h1>);  // output => 5
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+// Write code here:
