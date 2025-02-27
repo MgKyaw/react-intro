ad20dbc0c0818415e075af71cf496c2829d59ab6
MgKyaw <mgkyaw20112@gmail.com>
2025-02-27 23:13:55 +0700

    feat: Add code for JS in JSX

--- app.js
+++ app.js
@@ -77,3 +77,10 @@
 //       </article>
 //     </div>
 //   );
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+// Write code here:
