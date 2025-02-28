a0aba486f882c3e768f3458d978fc5def080bd89
MgKyaw <mgkyaw20112@gmail.com>
2025-02-28 21:29:38 +0700

    feat: Comment fixing to show correct string

--- app.js
+++ app.js
@@ -87,11 +87,11 @@
 // // root.render(<h1>2 + 3</h1>); // output => 2 + 3
 // root.render(<h1>{2 + 3}</h1>);  // output => 5
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-// Write code here:
-const math = <h1>2 + 3 = {2 + 3}</h1>;
-root.render(math);
+// const container = document.getElementById('app');
+// const root = createRoot(container);
+// // Write code here:
+// const math = <h1>2 + 3 = {2 + 3}</h1>;
+// root.render(math);
