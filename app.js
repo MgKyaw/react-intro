dbd31a94728d4600bdd253d77f50a4bf37b83e8a
MgKyaw <mgkyaw20112@gmail.com>
2025-02-28 23:16:16 +0700

    feat: Add code for event listener

--- app.js
+++ app.js
@@ -116,3 +116,23 @@
 // // Declare new variable here:
 // const gooseImg = <img src={goose} />;
 // root.render(gooseImg);
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img 
+		src="https://content.codecademy.com/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" />
+);
+
+root.render(null);
