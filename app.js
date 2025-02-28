f3f090892c11eed0c93e0a212a90195992a151ad
MgKyaw <mgkyaw20112@gmail.com>
2025-02-28 21:42:14 +0700

    feat: Add code for variables in JSX

--- app.js
+++ app.js
@@ -95,3 +95,13 @@
 // // Write code here:
 // const math = <h1>2 + 3 = {2 + 3}</h1>;
 // root.render(math);
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+const theBestString = 'This text was accessed through a javascript variable';
+
+root.render(___);
