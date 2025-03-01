a9960c7d93d61879f7c0cd48c4abaa50b106e5e4
MgKyaw <mgkyaw20112@gmail.com>
2025-03-01 11:18:47 +0700

    feat: Add code for ternary operator

--- app.js
+++ app.js
@@ -162,3 +162,22 @@
 // }
 
 // root.render(img);
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[x ? y : z]} />;
+
+root.render(img);
