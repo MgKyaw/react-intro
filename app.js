20c2cef0d47a9ca81cc1e2bc75ee781226c0ac90
MgKyaw <mgkyaw20112@gmail.com>
2025-03-01 11:38:23 +0700

    feat: Comment && operator

--- app.js
+++ app.js
@@ -183,24 +183,24 @@
 
 // root.render(img);
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-// judgmental will be true half the time.
-const judgmental = Math.random() < 0.5;
+// const container = document.getElementById('app');
+// const root = createRoot(container);
+// // judgmental will be true half the time.
+// const judgmental = Math.random() < 0.5;
 
-const favoriteFoods = (
-  <div>
-    <h1>My Favorite Foods</h1>
-    <ul>
-      <li>Sushi Burrito</li>
-      <li>Rhubarb Pie</li>
-      {!judgmental && <li>Nacho Cheez Straight Out The Jar</li>}
-      <li>Broiled Grapefruit</li>
-    </ul>
-  </div>
-);
+// const favoriteFoods = (
+//   <div>
+//     <h1>My Favorite Foods</h1>
+//     <ul>
+//       <li>Sushi Burrito</li>
+//       <li>Rhubarb Pie</li>
+//       {!judgmental && <li>Nacho Cheez Straight Out The Jar</li>}
+//       <li>Broiled Grapefruit</li>
+//     </ul>
+//   </div>
+// );
 
-root.render(favoriteFoods);
+// root.render(favoriteFoods);
