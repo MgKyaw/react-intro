0032d8a2e4bf78306549baac2defa44fed965601
MgKyaw <mgkyaw20112@gmail.com>
2025-03-01 11:35:14 +0700

    feat: Show li tag with && operator

--- app.js
+++ app.js
@@ -197,7 +197,7 @@ const favoriteFoods = (
     <ul>
       <li>Sushi Burrito</li>
       <li>Rhubarb Pie</li>
-      <li>Nacho Cheez Straight Out The Jar</li>
+      {!judgmental && <li>Nacho Cheez Straight Out The Jar</li>}
       <li>Broiled Grapefruit</li>
     </ul>
   </div>
