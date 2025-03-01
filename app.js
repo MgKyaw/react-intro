518eff2babe24eff9e10c02f252836db688fd644
MgKyaw <mgkyaw20112@gmail.com>
2025-03-01 11:33:14 +0700

    feat: Add code for && operator

--- app.js
+++ app.js
@@ -182,3 +182,25 @@
 // const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
 
 // root.render(img);
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+      <li>Nacho Cheez Straight Out The Jar</li>
+      <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+root.render(favoriteFoods);
